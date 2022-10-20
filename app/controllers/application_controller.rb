dc78c50ede3b27374f817595acf322b4c2bd9370
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-20 05:35:36 +0000

    [Add] ヘッダー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  private
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      about_path
+    else
+      root_path
+    end
+  end
 end
